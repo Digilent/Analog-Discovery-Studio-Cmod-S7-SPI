d4371501694b540b7a1b591eed1312d710544e63
ArtVVB <arthur.brown@digilent.com>
2019-07-10 15:30:58 -0700

    add BIN_FILE setting to project_info

--- project_info.tcl
+++ project_info.tcl
@@ -1,5 +1,5 @@
 # This is an automatically generated file used by digilent_vivado_checkout.tcl to set project options
-proc set_digilent_project_properties {proj_name} {
+proc set_project_properties_post_create_project {proj_name} {
     set project_obj [get_projects $proj_name]
 	set_property "part" "xc7s25csga225-1" $project_obj
 	set_property "board_part" "digilentinc.com:cmod-s7-25:part0:1.0" $project_obj
